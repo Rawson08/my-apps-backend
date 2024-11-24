13b5fd5dc4566efda278db4cc6ab9e918e5215b6
Roshan Subedi <81471729+Rawson08@users.noreply.github.com>
2024-11-24 14:13:06 -0700

    Update https-proxy-agent version

--- config/db.js
+++ config/db.js
@@ -5,36 +5,14 @@ const proxyUrl = process.env.QUOTAGUARDSTATIC_URL;
 const agent = new HttpsProxyAgent(proxyUrl);
 
 const connectDB = async () => {
-    // try {
-    //     await mongoose.connect(process.env.MONGO_URI, { httpAgent: agent });
-    //     console.log('Connected to MongoDB Atlas');
-    // } catch (error) {
-    //     console.error('Error connecting to MongoDB:', error);
-    //     process.exit(1);
-    // }
-
-
     try {
-        // Modify connection options for the agent
-        const connectionOptions = {
-            serverSelectionTimeoutMS: 5000,
-            directConnection: true,
-            driverInfo: {
-                name: "mongodb",
-                version: "4.1"
-            },
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            httpAgent: agent, // Pass the agent
-        };
-
-        await mongoose.connect(process.env.MONGO_URI, connectionOptions);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB Atlas');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
-        process.exit(1); // Exit process with failure
+        process.exit(1);
     }
+
 };
 
 module.exports = connectDB;
-
