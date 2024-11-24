cc8d4e1312dbe91e859e90306696a203e5476389
Roshan Subedi <81471729+Rawson08@users.noreply.github.com>
2024-11-24 13:50:31 -0700

    updated mongoDB and static ip for heroku

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const HttpsProxyAgent = require('https-proxy-agent');
+
+const proxyUrl = process.env.QUOTAGUARDSTATIC_URL;
+const agent = new HttpsProxyAgent(proxyUrl);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, { agent });
         console.log('Connected to MongoDB Atlas');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
